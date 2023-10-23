2bc97275554f1d4e45dd193170f1cdf141b65463
anapaulalemes <anapaulal.fredrich@gmail.com>
2023-10-22 23:33:42 -0300

    Adicionando o componente Inicio

---
+++ src/paginas/Inicio/index.js
@@ -0,0 +1,12 @@
+import Banner from "../../componentes/Banner";
+
+export default function Inicio() {
+    return (
+        <main>
+            <Banner />
+
+            <h1>Olá universo!</h1>
+            </main>
+
+    )
+}
