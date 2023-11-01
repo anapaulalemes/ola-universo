dec3c3035c7316411a2ed36c95a9cba7524a7f86
anapaulalemes <anapaulal.fredrich@gmail.com>
2023-10-31 23:51:50 -0300

    Modificacoes no sobre mim

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,29 @@
+import styles from './SobreMim.module.css'
+import PostModelo from "componentes/PostModelo";
+import fotoCapa from "assets/sobremimcapa.png"
+//import fotoSobreMim from "assets"
+
 export default function SobreMim() {
     return (
-        <h1>Sobre mim...</h1>
+        <PostModelo
+        fotoCapa={fotoCapa}
+        titulo="Sobre Mim"
+        >
+        
+        <h3 className={styles.subtitulo}>
+            Olá, eu sou a Ana!
+        </h3>
+
+        {/* //<img
+        src={fotoSobreMim}
+        alt="Foto da Ana Paula"
+        className={styles.fotoSobreMim}
+        /> */}
+        
+        <p className={styles.paragrafo}>
+            Hello
+        </p>
+
+        </PostModelo>
     )
 }
