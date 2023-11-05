20f740a2d03c20b993c2621e9b1ade9e08a3f110
anapaulalemes <anapaulal.fredrich@gmail.com>
2023-11-05 02:33:47 -0300

    fx: Modificando o routes

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,36 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
+import TextosAutorais from "paginas/TextosAutorais";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
-
-
+import Post from "paginas/Post";
+import NaoEncontrada from "paginas/NaoEncontrada";
+import ScrollToTop from "componentes/ScrollToTop";
+import InicioTextos from "paginas/InicioTextos";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Menu />
 
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
-          <Route  path="/" element={<Inicio />}/>
-          <Route path="/sobremim" element={<SobreMim />} />
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
+          <Route path="/textosautorais" element={<InicioTextos />} />
         </Route>
 
-        <Route path="*" element={<div>Página não encontrada</div>} />
+        <Route path="posts/:id/*" element={<Post />} />
+        <Route path="textosautorais/:id/*" element={<TextosAutorais />} />
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
       <Rodape />
     </BrowserRouter>
-  )
+  );
 }
 
 export default AppRoutes;
