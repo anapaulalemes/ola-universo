f7e3ce81561448e0a5efae8134b5f0d423eee462
anapaulalemes <anapaulal.fredrich@gmail.com>
2023-11-05 02:31:36 -0300

    fx: Modificando o index

--- src/componentes/PostCard/index.js
+++ src/componentes/PostCard/index.js
@@ -1,21 +1,20 @@
-import { Link } from 'react-router-dom'
-import styles from './PostCard.module.css'
-import BotaoPrincipal from 'componentes/BotaoPrincipal'
+import { Link } from "react-router-dom";
+import styles from "./PostCard.module.css";
+import BotaoPrincipal from "componentes/BotaoPrincipal";
 
+export default function PostCard({ post }) {
+  return (
+    <Link to={`/posts/${post.id}`}>
+      <div className={styles.post}>
+        <img
+          className={styles.capa}
+          src={require(`/public/posts/${post.id}/capa.png`)}
+          alt="Imagem de capa do post"
+        />
 
-export default function PostCard({post}) {
-    return (
-        <Link to={`/posts/${post.id}`}>
-             <div className={styles.post}>
-            <img
-             className={styles.capa}
-             src={require(`/public/posts/${post.id}/capa.png`)}
-             alt="Imagem de capa do post"
-             />
-
-             <h2 className={styles.titulo}>{post.titulo}</h2>
-             <BotaoPrincipal>Ler mais</BotaoPrincipal>
-        </div>
-        </Link>
-    )
+        <h2 className={styles.titulo}>{post.titulo}</h2>
+        <BotaoPrincipal>Ler mais</BotaoPrincipal>
+      </div>
+    </Link>
+  );
 }
